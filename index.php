70f8d085bc9b1a3d3d45f0a30af3af09bf4fc1d9
MirnaNika <136347480+MirnaNika@users.noreply.github.com>
2023-10-02 14:19:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,18 @@ public function __construct ($questionTxt, $answerOption, $correctAnswer) {
 public function displayQuestion() {
     echo "Questions: {$this->questionText}\n";
     echo "Choose the right answer: \n";
+
+    foreach ($this->answerOptions as $key => $option) {
+        echo "[$key] $option\n";
+} 
+}
+public function checkAnswer($selectedOptions) {
+    sort($selectedOptions);
+    sort($this->correctAnswer);
+
+    return $selectedOptions === $this->correctAnswer;
+}
+}
 } 
 }
 
