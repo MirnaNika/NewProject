51256bd5aebdf17401f767df8708558b0792d5d1
MirnaNika <136347480+MirnaNika@users.noreply.github.com>
2023-10-02 16:15:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,25 +25,20 @@ public function checkAnswer($selectedOptions) {
     return $selectedOptions === $this->correctAnswer;
 }
 }
+?>
 
+<?php
+$q1 = new Question("Which of these terms are 'access modifiers' in PHP?",
+["protected", "private", "public", "abstract"],
+    [0, 1, 2]);
 
+    $q1->displayQuestion();
+    $selectedOptions = [0, 1, 2];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if ($frage1->checkAnswer($selectedOptions)) {
+        echo "Right answer!\n";
+    } else {
+        echo "Wrong answer. Right answers are: 0, 1, 2\n";
+    }
+    
 ?>
