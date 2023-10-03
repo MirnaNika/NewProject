19bd6b745720275251dc1d7d50821ef1788898aa
MirnaNika <136347480+MirnaNika@users.noreply.github.com>
2023-10-03 09:17:23 +0200

    Update.php

--- index.php
+++ index.php
@@ -1,44 +1,45 @@
 <?php
-class Questions {
+class Question {
     private $questionTxt;
     private $answerOptions;
     private $correctAnswer;
 
-public function __construct ($questionTxt, $answerOption, $correctAnswer) {
-    $this-> $questionTxt = $questionTxt;
-    $this-> $answerOption = $answerOption;
-    $this-> $correctAnswer = $correctAnswer;
+    public function __construct($questionTxt, $answerOptions, $correctAnswer) {
+        $this->questionTxt = $questionTxt;
+        $this->answerOptions = $answerOptions;
+        $this->correctAnswer = $correctAnswer;
+    }
 
-}
-public function displayQuestion() {
-    echo "Questions: {$this->questionText}\n";
-    echo "Choose the right answer: \n";
+    public function displayQuestion() {
+        echo "Question: {$this->questionTxt}\n";
+        echo "Choose the right answer: \n";
 
-    foreach ($this->answerOptions as $key => $option) {
-        echo "[$key] $option\n";
-} 
-}
-public function checkAnswer($selectedOptions) {
-    sort($selectedOptions);
-    sort($this->correctAnswer);
+        foreach ($this->answerOptions as $key => $option) {
+            echo "[$key] $option\n";
+        }
+    }
 
-    return $selectedOptions === $this->correctAnswer;
-}
+    public function checkAnswer($selectedOptions) {
+        sort($selectedOptions);
+        sort($this->correctAnswer);
+
+        return $selectedOptions === $this->correctAnswer;
+    }
 }
 ?>
 
 <?php
 $q1 = new Question("Which of these terms are 'access modifiers' in PHP?",
-["protected", "private", "public", "abstract"],
+    ["protected", "private", "public", "abstract"],
     [0, 1, 2]);
 
-    $q1->displayQuestion();
-    $selectedOptions = [0, 1, 2];
+$q1->displayQuestion();
+$selectedOptions = [0, 1, 2];
 
-    if ($frage1->checkAnswer($selectedOptions)) {
-        echo "Right answer!\n";
-    } else {
-        echo "Wrong answer. Right answers are: 0, 1, 2\n";
-    }
-    
+if ($q1->checkAnswer($selectedOptions)) {
+    echo "Right answer!\n";
+} else {
+    echo "Wrong answer. Right answers are: 0, 1, 2\n";
+}
 ?>
+
